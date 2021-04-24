46400ef4de836880e981d3881222a26fe5fd99a5
Sahil Rajput <sahilrajput03@gmail.com>
2021-04-24 17:07:39 +0530

    Add things. ~ sahil

--- bcrypt-jwt/jwt.ts
+++ bcrypt-jwt/jwt.ts
@@ -1,9 +1,21 @@
 import {sign as makeToken, verify as decryptToken} from 'jsonwebtoken'
 // `npm i jsonwebtoken`
 
+const tokenIdentifiers = {
+	project_name: 'name of your project',
+	// You must use the different project_name to identify the project so that anytime later seeing any token you would know which token corresponds to which project.
+	// Also, this is helpful if you are using same SECRET in another project, then you it would be easy to identify the the source of user.
+	key_name: 'mykey1',
+	// You must change the key_name here if you change the SECRET for tokenization.
+	// Using key_name is useful to identify if you have a bunch of keys in your vault and you get to know which key can be used to decrypt the token.
+}
+
 const main = async () => {
 	const SECRET = 'some secret from .env file' // 10 is ok.
-	const username = {username: 'sahilrajput03'}
+	const username = {
+		username: 'sahilrajput03',
+		...tokenIdentifiers,
+	}
 	const token = await makeToken(username, SECRET)
 	console.log('tokenized username:')
 	console.log(token) // Output(changes on every execution): $2b$10$XLP4wLlXgILaXNOWJiJU6uusly/LQnl4DOe.cw3Eon003LQ0RWY/q
--- jwt-auth/server.ts
+++ jwt-auth/server.ts
@@ -19,8 +19,12 @@ app.post('/login', (req, res) => {
 	// authenticate user using bcrypt, easy-pasy.
 	const user = {name: req.body.username}
 
-	const accessToken = makeToken(user, ACCESS_TOKEN_SECRET)
-	res.json({accessToken})
+	// const token = makeToken(user, ACCESS_TOKEN_SECRET) // original.
+	// be default HS256 encryption is used to make a new token if you don't pass algorithm to `jwt.sign` method, which is really good though.
+	const token = makeToken(user, ACCESS_TOKEN_SECRET, {
+		algorithm: 'none',
+	}) // original.
+	res.json({token})
 })
 
 function authenticateToken(req, res, next) {
@@ -40,7 +44,8 @@ function authenticateToken(req, res, next) {
 		decodedToken = decryptToken(token, ACCESS_TOKEN_SECRET)
 		// Since, getToken(jwt.verify) throws error if token is invalid, we must use try/catch around it.
 	} catch (error) {
-		// log("can't decode token");
+		log("can't decode token")
+		log(error)
 		return res.status(403).send('Invalid token')
 	}
 
